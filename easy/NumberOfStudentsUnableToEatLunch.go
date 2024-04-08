0ef224bbbef84e5ea9e7016705a28f8d19e652da
Trajche <trajchemichov@gmail.com>
2024-04-08 10:53:19 +0200

    Daily, easy 1700. Num of students unable to eat lunch

---
+++ easy/NumberOfStudentsUnableToEatLunch.go
@@ -0,0 +1,19 @@
+package easy
+
+// 1700. Number of students unable to eat lunch, daily 
+func NumberOfStudentsUnableToEatLunch(students []int, sandwiches []int) int {
+	countPreferences := make(map[int]int)
+
+	for _, preference := range students {
+		countPreferences[preference]++
+	}
+
+	for _, sandwich := range sandwiches {
+		if countPreferences[sandwich] == 0 {
+			break
+		}
+		countPreferences[sandwich]--
+	}
+
+	return countPreferences[0] + countPreferences[1]
+}

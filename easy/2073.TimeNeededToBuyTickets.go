a425c2bde75665ec0daa63c7f885ef1175c0e431
Trajche <trajchemichov@gmail.com>
2024-04-09 20:08:51 +0200

    2073.Time needed to buy tickets

---
+++ easy/2073.TimeNeededToBuyTickets.go
@@ -0,0 +1,16 @@
+package easy
+
+func TimeNeededToBuyTickets (tickets []int, k int) int {
+	target := tickets[k]
+	seconds := 0
+
+	for i, v := range tickets {
+		if i <= k {
+			seconds += min(v, target)
+		} else {
+			seconds += min(v, target-1)
+		}
+	}
+
+	return seconds
+}

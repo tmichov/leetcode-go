0009e914a846a667de2c002bb501aa1557af744b
Trajche <trajchemichov@gmail.com>
2024-05-03 12:50:26 +0200

    Easy 111. Minimum depth of binary tree

---
+++ easy/111.MinimumDepthOfBinaryTree.go
@@ -0,0 +1,29 @@
+package easy
+
+func MinimumDepthOfBinaryTree(root *TreeNode) int {
+    if root == nil {
+		return 0
+	}
+
+	leftDepth := MinimumDepthOfBinaryTree(root.Left)
+	rightDepth := MinimumDepthOfBinaryTree(root.Right)
+
+    return 1+min(leftDepth, rightDepth)
+}
+
+func min(leftDepth, rightDepth int) int {
+    if leftDepth == 0 {
+		return rightDepth
+	}
+
+	if rightDepth == 0 {
+		return leftDepth
+	}
+
+	if leftDepth < rightDepth {
+		return leftDepth
+	}
+
+	return rightDepth
+}
+
--- main.go
+++ main.go
@@ -11,16 +11,13 @@ func main() {
 		Val: 1,
 	}
 
-	node.Left = &easy.TreeNode{Val: 2}
 	node.Right = &easy.TreeNode{Val: 2}
+	node.Left = &easy.TreeNode{Val: 2}
 
-	node.Left.Left = &easy.TreeNode{Val: 2}
-
+	node.Right.Left = &easy.TreeNode{Val: 2}
 	node.Right.Right = &easy.TreeNode{Val: 2}
 
-	node.Left.Left.Left = &easy.TreeNode{Val: 2}
-	node.Right.Right.Right = &easy.TreeNode{Val: 2}
-	balanced := easy.IsBalanced(node)
+	balanced := easy.MinimumDepthOfBinaryTree(node)
 
 	fmt.Println("balanced: ", balanced)
 }

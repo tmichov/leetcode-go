ab4ff8c314688e0e2168464e842a135348247afe
Trajche <trajchemichov@gmail.com>
2024-05-05 14:05:41 +0200

    Easy 141. Linked list cycle

--- util/PrintLinkedList.go
+++ util/PrintLinkedList.go
@@ -1,6 +1,8 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func PrintLinkedList(root *ListNode) {
 	if root == nil {
@@ -9,6 +11,25 @@ func PrintLinkedList(root *ListNode) {
 
 	fmt.Println(root.Val)
 	PrintLinkedList(root.Next)
+}
+
+func GenerateLinkedList(nums []int) (*ListNode, *ListNode) {
+	var root *ListNode
+	var curr *ListNode
+
+	for i := 0; i < len(nums); i++ {
+		if i == 0 {
+			root = &ListNode{Val: nums[i]}
+			curr = root
+			continue
+		}
+
+		curr.Next = &ListNode{Val: nums[i]}
+
+		curr = curr.Next
+	}
+
+	return root, curr
 }
 
 func PrintBinarySearchTree(head *TreeNode) {

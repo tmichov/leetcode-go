88265d09ae7c4ef9a503a28d26a51ef911d23231
Trajche <trajchemichov@gmail.com>
2024-05-05 14:05:41 +0200

    Easy 141. Linked list cycle

---
+++ easy/141.LinkedListCycle.go
@@ -0,0 +1,23 @@
+package easy
+
+import "github.com/tmichov/leetcode/util"
+
+func LinkedListCycle(head *util.ListNode) bool {	
+	if head == nil {
+		return false
+	}
+
+	fast := head.Next
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		if slow == fast {
+			return true
+		}
+
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return false
+}
--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 
 	"github.com/tmichov/leetcode/easy"
+	"github.com/tmichov/leetcode/util"
 )
 
 
 func main() {
-	test := easy.ValidPalindrome(" ")
+	root, last := util.GenerateLinkedList([]int{1, 1, 2, 2, 3, 4, 5, 6})
+
+	last.Next = root.Next
+
+	test := easy.LinkedListCycle(root)
 
 	fmt.Println(test)
 }
